c4e835179552a9285cf7ceb93d2ead337bb2ce84
Devorah Ortega <devorahb.ortega@gmail.com>
2022-06-27 16:50:10 -0500

    Commits class work for Class 1

---
+++ 03.py
@@ -0,0 +1,12 @@
+# this program computes the sum of 
+# all numbers from 1 - 100
+s = 0
+for i in range(100):
+	s = s + i + 1
+print(s)
+# this program computes the sum of 
+# all numbers from 1 - 1000
+s = 0
+for i in range(1000):
+	s = s + i + 1
+print(s)
