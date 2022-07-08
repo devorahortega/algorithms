19d461296a315ea59ada11d5e97aae78ccf235c7
Devorah Ortega <devorahb.ortega@gmail.com>
2022-07-08 10:03:17 -0500

    Adds more info on modules

--- 07.py
+++ 07.py
@@ -1,7 +1,11 @@
 array = [[],[]]
 
-for i in range(999, 9000):
+for i in range(999, 1010):
   array[0].append(str(i + 1))
-for items in array[0]:
-  array[1].append('a' + items) 
+
+
+for item in array[0]:
+  for k in range(len(item) + 1):
+    array[1].append(item[0:k] + 'a' + item[k:len(item)])
+
 print(array)
