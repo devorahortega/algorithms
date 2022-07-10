56dc5a4d79cc3d5cfdadac8f85bc4b2d1f2f8f71
Devorah Ortega <devorahb.ortega@gmail.com>
2022-07-10 16:39:04 -0500

    Adds more attempts

--- Practice.py
+++ Practice.py
@@ -1,11 +1,13 @@
 array = [[],[]]
 
-for i in range(999, 1010):
+for i in range(1, 9):
   array[0].append(str(i + 1))
 
 
 for item in array[0]:
   for k in range(len(item) + 1):
-    array[1].append(item[0:k] + 'a' + item[k:len(item)])
+    array[1].append(item[1:k] + '2' + item[k:len(item)])
+  for k in range(len(item) + 1):
+    array[1].append(item[2:k] + '3' + item[k:len(item)])
 
 print(array)
