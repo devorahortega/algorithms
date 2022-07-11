c59815b7c9bd9cf8d9343d6abe8e7749fc925cc1
Devorah Ortega <devorahb.ortega@gmail.com>
2022-07-11 13:54:04 -0500

    Adds 2nd homework to repository

---
+++ HW1.py
@@ -0,0 +1,12 @@
+""" 
+Write a program that computes the square root of 1234556789012345678910
+and find how many steps it takes so as to achieve a precision of 20 dec-
+imal digits.
+
+Write a program that computes the square roots of all numbers from
+1000000 to 2000000 and records the number of steps it takes for each
+computation to reach a precision of 20 decimal digits.
+
+Write a program that computes all the square roots of all six digit
+numbers that are made up of the digits 1,2,3,4,5,6 (in any order).
+"""
