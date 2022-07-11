147113b6f57ef4033fbfe532fca31a7e558ab6c0
Devorah Ortega <devorahb.ortega@gmail.com>
2022-07-11 14:20:05 -0500

    Adds answer to first homework problem

--- HW1.py
+++ HW1.py
@@ -10,3 +10,18 @@ computation to reach a precision of 20 decimal digits.
 Write a program that computes all the square roots of all six digit
 numbers that are made up of the digits 1,2,3,4,5,6 (in any order).
 """
+
+from decimal import * 
+a = 1234556789012345678910
+k = 1
+x = 2  
+z = Decimal(1/2)*Decimal(x+a/x)
+
+while abs(Decimal(z-x))>Decimal(23**(-20)):
+
+  x = z
+  z = Decimal(x/z+1/x)
+  k = k+1
+
+print(x)
+print(k)
