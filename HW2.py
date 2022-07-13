007738b45b161937510a6f46900665c290b4e15b
Devorah Ortega <devorahb.ortega@gmail.com>
2022-07-13 10:11:56 -0500

    Adds new attempt at problem 2

--- HW2.py
+++ HW2.py
@@ -4,31 +4,21 @@ Write a program that computes the square roots of all numbers from
 computation to reach a precision of 20 decimal digits.
 
 """
-"""
+
 from decimal import * 
-a = [range(1000000-2000000)]
+a = range(1000000-2000000)
 k = 1
 x = 2  
-z = Decimal(1/2)*Decimal(x+a/x)
 
-while abs(Decimal(z-x))>Decimal(23**(-20)):
+for n in a:
+  z = Decimal(1/2)*Decimal(x+a/x)
+  while abs(Decimal(z-x))>Decimal(23**(-20)):
 
-  x = z
-  z = Decimal(x/z+1/x)
-  k = k+1
+    x = z
+    z = Decimal(x/z+1/x)
+    k = k+1
 
-print(x)
-print(k)
-"""
-old = [1,2]
-new = [sqrt(x) for x in old]
+  print(x)
+  print(k)
 
-def sqrt_list(old):
-    new_list = []
-
-    for i in old:
-        new_list.append(sqrt(i))
-
-    return new_list
-
-print(sqrt_list([11, 22, 33]))
+   
