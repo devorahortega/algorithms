f40128edf91e054fa0a6b0df13d5aaff8fda8ab2
Devorah Ortega <devorahb.ortega@gmail.com>
2022-07-13 10:19:40 -0500

    Adds attempt to solve problem 3

--- HW3.py
+++ HW3.py
@@ -3,9 +3,20 @@ Write a program that computes all the square roots of all six digit
 numbers that are made up of the digits 1,2,3,4,5,6 (in any order).
 
 """
-
 from decimal import * 
-a = range(1000000-2000000)
+
+l = 5
+permutations = []
+permutations.append(['1'])
+for n in range(l-1):
+  permutations.append([])
+for n in range(l-1): 
+  digit = str(n+2)
+  for item in permutations[n]:
+    for k in range(len(item)+1):
+      permutations[n+1].append(item[0:k]+digit+item[k:len(item)])
+
+a = permutations
 k = 1
 x = 2  
 
