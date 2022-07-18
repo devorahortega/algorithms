90e9440365218b6150722be1011741989a9db027
Devorah Ortega <devorahb.ortega@gmail.com>
2022-07-17 19:48:37 -0500

    Adds advancing

--- 11.py
+++ 11.py
@@ -5,3 +5,20 @@ for i in range(n):
   numbers.append(random.randrange(1,100000))
 
 print(numbers)
+
+for i in range(n):
+  done=False
+  j = i-1
+  while(done==False):
+    if j<0:
+      done=True
+    else:
+      if numbers[j+1]>numbers[j]:
+        a = numbers[j]
+        numbers[j] = numbers[j+1]
+        numbers[j+1] = a
+        j=j-1
+      else:
+         done=True
+
+print(numbers)
