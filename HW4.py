85e709645a257a372a9efa92891e1ca65273d152
Devorah Ortega <devorahb.ortega@gmail.com>
2022-07-20 09:03:10 -0500

    Adds base template to homework

--- HW4.py
+++ HW4.py
@@ -8,4 +8,28 @@ need to look up the generation of random string and this may entail
 using an extra library.
 
 """
-   
+  
+import random
+n = 100
+numbers = []
+for i in range(n):
+  numbers.append(random.randrange(1,100000))
+
+print(numbers)
+
+for i in range(n):
+  done=False
+  j = i-1
+  while(done==False):
+    if j<0:
+      done=True
+    else:
+      if numbers[j+1]>numbers[j]:
+        a = numbers[j]
+        numbers[j] = numbers[j+1]
+        numbers[j+1] = as
+        j=j-1
+      else:
+         done=True
+
+print(numbers)
