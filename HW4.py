f8201dce0c58f9bcf6725e68083d212107c6d10e
Devorah Ortega <devorahb.ortega@gmail.com>
2022-07-19 19:54:01 -0500

    Adds homework for today

---
+++ HW4.py
@@ -0,0 +1,11 @@
+"""
+Write a program that generates 100 random strings of length 10 con-
+sisting of lower case letters and then uses a bubble sort algorithm in
+order to sort them alphabetically. Note that in Python the comparison
+of strings is done exactly as the comparison of numbers and a sting is
+less than another one if it would precede it in the dictionary. You will
+need to look up the generation of random string and this may entail
+using an extra library.
+
+"""
+   
