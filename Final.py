e0315c1d44ebcd53150379e2c612b2f4f5226098
Devorah Ortega <devorahb.ortega@gmail.com>
2022-07-28 13:14:04 -0500

    Adds more code

--- Final.py
+++ Final.py
@@ -10,3 +10,29 @@ for n in range(l-1):
       permutations[n+1].append(item[0:k]+digit+item[k:len(item)])
 for item in permutations[l-1]:
   print(item)
+
+letters = string.ascii_lowercase 
+n = 100
+strings = []
+
+for i in range(n):
+  strings.append(''.join(random.choice(letters)for i in range(10)))
+
+print(strings)
+
+for i in range(n):
+  done=False
+  j = i-1
+  while(done==False):
+    if j<0:
+      done=True
+    else:
+      if strings[j+1]>strings[j]:
+        a = strings[j]
+        strings[j] = strings[j+1]
+        strings[j+1] = a
+        j=j-1
+      else:
+         done=True
+
+print(strings)
